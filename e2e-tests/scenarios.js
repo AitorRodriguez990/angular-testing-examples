c34106caa046ab25761d8a6c0a1ef4bc55be6faa
Aitor Rodríguez <aitor.rodriguez990@gmail.com>
2016-09-30 10:50:42 +0200

    Changes on e2e tests to apply changes on application

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -1,42 +1,30 @@
 'use strict';
 
-/* https://github.com/angular/protractor/blob/master/docs/toc.md */
-
-describe('my app', function() {
-
-
-  it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
+describe('My App', function() {
+  it('should automatically redirect to /home when location hash/fragment is empty', function() {
     browser.get('index.html');
-    expect(browser.getLocationAbsUrl()).toMatch("/view1");
+    expect(browser.getLocationAbsUrl()).toMatch("/home");
   });
 
-
-  describe('view1', function() {
-
+  describe('Home', function() {
     beforeEach(function() {
-      browser.get('index.html#!/view1');
+      browser.get('index.html#!/home');
     });
 
-
-    it('should render view1 when user navigates to /view1', function() {
+    it('should render home when user navigates to /home', function() {
       expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 1/);
+        toMatch(/Hello World!/);
     });
-
   });
 
-
-  describe('view2', function() {
-
+  describe('Contact', function() {
     beforeEach(function() {
-      browser.get('index.html#!/view2');
+      browser.get('index.html#!/contact');
     });
 
-
-    it('should render view2 when user navigates to /view2', function() {
+    it('should render contact when user navigates to /contact', function() {
       expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 2/);
+        toMatch(/This is the partial for Contact/);
     });
-
   });
 });
