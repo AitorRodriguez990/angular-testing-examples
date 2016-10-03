902a1d9946864c11da8e982d626e4d6393e6fb21
Aitor Rodríguez <aitor.rodriguez990@gmail.com>
2016-10-03 13:26:33 +0200

    Add more tests cases and sleeps to view tests slowly

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -1,30 +1,48 @@
 'use strict';
 
 describe('My App', function() {
-  it('should automatically redirect to /home when location hash/fragment is empty', function() {
-    browser.get('index.html');
-    expect(browser.getLocationAbsUrl()).toMatch("/home");
-  });
-
-  describe('Home', function() {
-    beforeEach(function() {
-      browser.get('index.html#!/home');
+    it('should automatically redirect to /home when location hash/fragment is empty', function() {
+        browser.get('index.html');
+        browser.sleep(3000);
+        expect(browser.getLocationAbsUrl()).toMatch("/home");
     });
 
-    it('should render home when user navigates to /home', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/Hello World!/);
-    });
-  });
-
-  describe('Contact', function() {
-    beforeEach(function() {
-      browser.get('index.html#!/contact');
+    describe('Home', function() {
+        it('should render home when user navigates to /home', function() {
+            expect(element.all(by.css('[ng-view] h3')).first().getText()).
+            toMatch(/Home/);
+        });
     });
 
-    it('should render contact when user navigates to /contact', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/This is the partial for Contact/);
+    describe('Contact', function() {
+        it('should render contact when user navigates to /contact', function() {
+            browser.get('index.html#!/contact');
+            browser.sleep(3000);
+            expect(element.all(by.css('[ng-view] h3')).first().getText()).
+            toMatch(/Contact/);
+        });
+
+        it('should write on inputs and read a success message', function() {
+            var name = element(by.model('name'));
+            name.clear();
+            name.sendKeys('Aitor');
+            browser.sleep(1000);
+
+            var surname = element(by.model('surname'));
+            surname.clear();
+            surname.sendKeys('Rodríguez');
+            browser.sleep(1000);
+
+            var company = element(by.model('company'));
+            company.clear();
+            company.sendKeys('Pisos.com');
+            browser.sleep(1000);
+
+            element(by.css('.btn-primary.send-contact')).click();
+            expect(element.all(by.css('[ng-view] p')).first().getText()).
+            toMatch(/Contact form sended successfully. Wait the reply now./);
+
+            browser.sleep(5000);
+        });
     });
-  });
 });
