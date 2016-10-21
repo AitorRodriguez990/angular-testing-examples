99c3bd9bdb319d2203ab4d5a66f2c5f9a9b0d859
Aitor Rodríguez <aitor.rodriguez990@gmail.com>
2016-10-21 11:34:33 +0200

    Update version

--- app/components/version/version_test.js
+++ app/components/version/version_test.js
@@ -5,7 +5,7 @@ describe('myApp.version module', function() {
 
     describe('Version service', function() {
         it('should return current version', inject(function(version) {
-            expect(version).toEqual('0.2');
+            expect(version).toEqual('0.3');
         }));
     });
 });
